8d255ed4605af4c2e2bfde25f93790ef80983a3a
GONGTING520 <954160311@qq.com>
2018-04-16 22:16:55 +0800

    引入person1-4导出的模块，并调用

---
+++ prictace1.js
@@ -0,0 +1,43 @@
+/*
+1.
+
+var person = require('./person1.js');
+
+console.log(person);
+var p = new person();
+p.eat();
+p.say();
+*/
+
+
+
+/*
+// 2.
+
+var person = require('./person2.js');
+
+console.log(person);
+person.eat();
+person.say();*/
+
+
+
+
+/*
+3.
+
+var person = require('./person3.js');
+
+console.log(person);
+person.eat();
+person.say();
+*/
+
+
+
+// 4.
+
+var person = require('./person4.js');
+
+console.log(person);
+console.log(person.arr);
